3d48568aae4a438932a292746bb4a6ecbedfd7cf
jay1846 <jay9809012@gmail.com>
2026-02-13 20:07:20 +0100

    5.c

---
+++ ch03/programing_projects/5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+	int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p;
+
+	printf("Enter the numbers from 1 to 16 in any order: \n");
+	scanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d", &a,&b,&c,&d,&e,&f,&g,&h,&i,&j,&k,&l,&m,&n,&o,&p);
+
+	printf("%3d %3d %3d %3d\n%3d %3d %3d %3d\n%3d %3d %3d %3d\n%3d %3d %3d %3d\n",a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p);
+
+	return 0;
+}
