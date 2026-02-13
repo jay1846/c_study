acf35eb1441e634b65e6c5056c03a68334e1cb71
jay1846 <jay9809012@gmail.com>
2026-02-13 21:49:38 +0100

    first version

---
+++ ch04/1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+
+	int first_digit, second_digit;
+	printf("Enter a two-digit number: ");
+	scanf("%d%d",&first_digit,&second_digit);
+
+	printf("Teh reversal is : %d%d\n", second_digit, first_digit);
+
+	return 0;
+}
+
