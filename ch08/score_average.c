e90a6275663af64cf8d8956c36a710ceb761ea9a
jay1846 <jay9809012@gmail.com>
2026-03-20 15:26:38 +0000

    feat: grade average from students by using array

---
+++ ch08/score_average.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define STUDENTS 5
+
+int main(void){
+    int scores[STUDENTS];
+    int sum = 0;
+    int i, average;
+
+    for(i = 0; i < STUDENTS; i++){
+        printf("Enter grades from students: ");
+        scanf("%d", &scores[i]);
+    }
+
+    for(i = 0; i < STUDENTS; i++){
+        sum += scores[i];
+    }
+
+    average = sum / STUDENTS;
+    printf("Average: %d\n", average);
+
+    return 0;
+}
