3811015959b1c0d77984867a93260d22e3e8998f
jay1846 <jay9809012@gmail.com>
2026-03-26 09:50:49 +0000

    feat: return more than 2 values by using pointer parameter

--- ch11/slope.c
+++ ch11/slope.c
@@ -13,5 +13,13 @@ int get_line_parameter(int x1, int y1, int x2, int y2, float *slope, float *yint
 }
 
 int main(void){
-    
+    float s, y;
+
+    if (get_line_parameter(3, 3, 6, 6, &s,  &y) == -1){
+        printf("Error\n");
+    }
+    else{
+        printf("slope: %.2f\ny intercept: %.2f\n", s, y);
+    }
+    return 0;
 }
