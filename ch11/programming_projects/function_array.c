6abc3d93aa457a890416ccad5fd99c827ac48411
jay1846 <jay9809012@gmail.com>
2026-03-27 05:34:13 +0000

    feat: get max logic to iterate through entire array

---
+++ ch11/programming_projects/function_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+double get_max(double *A, int size);
+
+int main(void){
+    double A[] = {1.23, 3.14, 9.16, 100.90};
+    double result = get_max(A, 4);
+    printf("Max vlaue is: %.2f\n", result);
+    return 0;
+}
+
+double get_max(double *A, int size){
+    double max = A[0];
+    for(int i = 1; i < size; i++){
+        if(max < A[i]){
+            max = A[i];
+        }
+    }
+    return max;
+}
