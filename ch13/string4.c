3884efda410e84a8c74e1b44472d82a4172733ed
jay1846 <jay9809012@gmail.com>
2026-03-27 07:12:30 +0000

    feat: calculate string length by detecting null terminator

---
+++ ch13/string4.c
@@ -0,0 +1,14 @@
+// Count length of string
+#include <stdio.h>
+
+int main(void){
+    char str[30] = "C language is easy";
+    int i = 0;
+
+    while(str[i] != '\0'){
+        i++;
+    }
+    printf("length of string \"%s\" is %d.\n", str, i);
+
+    return 0;
+}
