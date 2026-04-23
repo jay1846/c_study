29346188b3098b847f217602feb197743223e7f8
장재완 <jay@MacBookAir.local>
2026-04-23 12:42:50 +0200

    feat: two different pointer variables point same variable. * instead &

---
+++ ch11/pointer_refer_other_pointer.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int wert = 10;
+    int *ptr1 = &wert;
+    int *ptr2;
+
+    ptr2 = ptr1;
+
+    printf("ptr1 verweist auf %p.\n", ptr1);
+    printf("Wert in %p ist %d.\n", ptr1, *ptr1);
+
+    printf("ptr2 verweist auf %p.\n", ptr2);
+    printf("Wert in %p ist %d.\n", ptr2, *ptr2);
+
+    printf("Addresse von ptr1 : %p.\n", &ptr1);
+    printf("Addresse von ptr2 : %p.\n", &ptr2);
+    printf("Addresse von wert : %p.\n\n", &wert);
+
+    printf("ptr1 [%p] -> [%p] = [%d].\n", &ptr1, ptr1, *ptr1);
+    printf("ptr2 [%p] -> [%p] = [%d].\n", &ptr2, ptr2, *ptr2);
+
+    return EXIT_SUCCESS;
+}
